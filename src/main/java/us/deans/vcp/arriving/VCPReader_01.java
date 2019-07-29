a5a6f8b878b58dfea6603d4402d739be63c5add3
ndeans <ncdeans@gmail.com>
2019-07-28 23:04:23 -0400

    VCP - g2 upgrade

--- src/main/java/us/deans/vcp/arriving/VCPReader_01.java
+++ src/main/java/us/deans/vcp/arriving/VCPReader_01.java
@@ -2,7 +2,7 @@ package us.deans.vcp.arriving;
 
 public class VCPReader_01 implements VCPReader {
 
-	static String message = "Hello World!";
+	static String message = "This is the second generation upgrade to the reader";
 	
 	public String readArriving() {
 		return message;
