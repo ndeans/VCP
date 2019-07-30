2c0b56b6148a1cfb99bcb7b281e24be1bbd6223e
ndeans <ncdeans@gmail.com>
2019-07-30 00:18:43 -0400

    VCP adding Log4J logging

--- src/main/java/us/deans/vcp/departing/VCPWriter_01.java
+++ src/main/java/us/deans/vcp/departing/VCPWriter_01.java
@@ -1,9 +1,16 @@
 package us.deans.vcp.departing;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import us.deans.vcp.main.Application;
+
 public class VCPWriter_01 implements VCPWriter {
 
+	private Logger logger = LogManager.getLogger(Application.class.getName());
+	
 	public void writeDeparting(String msg) {
-		System.out.println(msg);
+		logger.info(msg);
 	}
 
 }
--- src/main/java/us/deans/vcp/main/Application.java
+++ src/main/java/us/deans/vcp/main/Application.java
@@ -1,18 +1,27 @@
 package us.deans.vcp.main;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 import us.deans.vcp.arriving.VCPReader;
 import us.deans.vcp.arriving.VCPReader_01;
 import us.deans.vcp.departing.VCPWriter;
 import us.deans.vcp.departing.VCPWriter_01;
 
+
 public class Application {
 
+	private static final Logger logger = LogManager.getLogger(Application.class.getName());
+	
 	public static void main(String[] args) {
 
 		String message;
 		
 		VCPReader reader = new VCPReader_01();
 		VCPWriter writer = new VCPWriter_01();
+
+		logger.info("Starting Test...");
 		
 		message = reader.readArriving();
 		writer.writeDeparting(message);
